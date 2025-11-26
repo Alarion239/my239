01728131b5755ba1f0097ee3f7565e4a80b78d90
Alexander Belotserkovtsev <amb30239@gmail.com>
2025-11-26 16:19:16 -0500

    Remove main.go file from backend directory

--- backend/cmd/telegram-bot/main.go
+++ backend/cmd/telegram-bot/main.go
@@ -9,12 +9,13 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8000" // Default to port 8000
+		port = "8000"
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Hello, World!</h1>")
 	})
 
+	fmt.Println("Server is running on port", port)
 	http.ListenAndServe(":"+port, nil)
 }
